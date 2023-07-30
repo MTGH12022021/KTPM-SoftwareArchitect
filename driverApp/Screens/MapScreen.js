b6c4bab562b113042cdf50b2d483427928ebf783
Chamelsqr999 <lequockyanh99@gmail.com>
2023-07-30 18:12:24 +0700

    driverapp (not done yet)

---
+++ driverApp/Screens/MapScreen.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import tw, { create } from "twrnc";
+import Map from "../components/Map";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionCard from "../components/RideOptionCard";
+
+const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <View>
+      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionCard"
+            component={RideOptionCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          {/* <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          /> */}
+        </Stack.Navigator>
+      </View>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
